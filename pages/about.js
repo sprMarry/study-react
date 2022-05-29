2a001d54f275fbd04da08bac761722ccbf452871
shimario <7890mario@gmail.com>
2022-05-29 17:53:52 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Headline } from '../components/Headline'
 import { Links } from '../components/Links'
 import { Footer } from '../components/Footer'
 
@@ -13,13 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>About Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/about.js</code>
-        </p>
-
+        <Headline page="about" />
         <Links/>
       </main>
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Headline } from '../components/Headline'
 import { Links } from '../components/Links'
 import { Footer } from '../components/Footer'
 
@@ -13,17 +14,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Index Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <Headline page="index" />
         <Links/>
       </main>
 
-      <Footer/>
+      <Footer />
     </div>
   )
 }
