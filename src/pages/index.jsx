767098117ebc6a6b2cf0e57d1c8e983e9e21a86d
shimario <7890mario@gmail.com>
2022-06-18 22:26:24 +0900

    ⚗️ クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,30 @@ import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import { Footer } from 'src/components/Footer';
+import { useCallback } from 'react';
 
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    const foo = 1;
+
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
-      <Header/>
+      <Header />
+      <a
+        href='/about'
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
