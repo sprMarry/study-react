a8feb81dbb94750b98894ac1aafaa08b99d3518e
shimario <7890mario@gmail.com>
2022-07-27 21:46:28 +0900

    ✨ ページ間のステートのリフトアップについての勉強

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,24 +3,28 @@ import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import { Footer } from 'src/components/Footer';
-import { useCounter } from 'src/hooks/useCounter';
-import { useInputArray } from 'src/hooks/useInputArray';
-import { useBgLightBlue } from 'src/hooks/useBgLightBlue';
 
-export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
-  useBgLightBlue();
-
+export default function About({
+  count,
+  isShow,
+  handleClick,
+  handleDisplay,
+  text,
+  array,
+  handleChange,
+  handleAdd,
+}) {
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
+
       <button onClick={handleAdd}>追加</button>
       <input type="text" value={text} onChange={handleChange} />
       <ul>
@@ -30,6 +34,7 @@ export default function About() {
           )
         })}
       </ul>
+
       <Main page="about" />
       <Footer/>
     </div>
