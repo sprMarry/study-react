d1ca21d66fefc68b7f467ca000c8c35ab14a26f6
shimario <7890mario@gmail.com>
2022-07-28 03:02:12 +0900

    ✨ コンポーネント間のリフトアップの勉強

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -5,8 +5,9 @@ export function Headline(props) {
     <div>
       <h1 className={classes.title}>{props.page} Page</h1>
       <p className={classes.description}>
-        Get started by editing {props.children}
+        アイテムの数は {props.children} です
       </p>
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   )
 }
